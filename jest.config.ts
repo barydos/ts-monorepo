00a60893e09e3afbdb5d8eab5162b9e2ba2bdab3
Barry Wu <barryxwu@gmail.com>
2024-10-13 16:24:37 +1100

    chore(lint): set print width to 100 instead of 80

--- jest.config.ts
+++ jest.config.ts
@@ -4,10 +4,7 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  ],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
